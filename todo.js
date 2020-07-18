1d33ed9b32a5a9706f64c8708a7af39c89ebb52a
frogbam <frogbam07@gmail.com>
2020-07-18 23:10:22 +0900

    add delete btn

--- todo.js
+++ todo.js
@@ -1,16 +1,27 @@
 const todoContainaer = document.querySelector(".js-todo");
 const todoForm = todoContainaer.querySelector("form");
 const todoInput = todoForm.querySelector("input");
-todoForm.addEventListener("submit", addTodo);
 
-function addTodo() {
-  const todo = todoInput.value;
-  const span = document.createElement("span");
-  const text = document.createTextNode(todo);
-  todoContainaer.appendChild(span);
-  span.appendChild(text);
+function onSubmit(event) {
+  event.preventDefault();
+  const value = todoInput.value;
+  todoInput.value = "";
+  addTodo(value);
+}
+
+function addTodo(todo) {
+  const div = document.createElement("div");
+  const deleteBtn = document.createElement("span");
+  deleteBtn.innerHTML = "❌";
+  deleteBtn.addEventListener("click", deleteTodo);
+  div.appendChild(deleteSpan);
+
+  div.innerHTML += todo;
+
+  todoContainaer.appendChild(div);
 }
 
 function init() {}
 
+todoForm.addEventListener("submit", onSubmit);
 init();
